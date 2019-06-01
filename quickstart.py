8eca80e634c35650be8f29bc548e4bcf8309ddd5
maynard-olegario <maynard.olegario@distilnetworks.com>
2019-06-01 00:09:06 -0700

    update with latest working quickstart

--- quickstart.py
+++ quickstart.py
@@ -1,74 +1,51 @@
-
 from __future__ import print_function
-import httplib2
-import os
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/presentations.readonly']
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/slides.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/presentations.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Slides API Python Quickstart'
-
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'slides.googleapis.com-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+# The ID of a sample presentation.
+PRESENTATION_ID = '1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc'
 
 def main():
     """Shows basic usage of the Slides API.
-
-    Creates a Slides API service object and prints the number of slides and
-    elements in a sample presentation:
-    https://docs.google.com/presentation/d/1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc/edit
+    Prints the number of slides and elments in a sample presentation.
     """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('slides', 'v1', http=http)
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-    presentationId = '1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc'
+    service = build('slides', 'v1', credentials=creds)
+
+    # Call the Slides API
     presentation = service.presentations().get(
-        presentationId=presentationId).execute()
+        presentationId=PRESENTATION_ID).execute()
     slides = presentation.get('slides')
 
-    print ('The presentation contains {} slides:'.format(len(slides)))
+    print('The presentation contains {} slides:'.format(len(slides)))
     for i, slide in enumerate(slides):
-        print('- Slide #{} contains {} elements.'.format(i + 1,
-            len(slide.get('pageElements'))))
+        print('- Slide #{} contains {} elements.'.format(
+            i + 1, len(slide.get('pageElements'))))
+
 
 if __name__ == '__main__':
     main()
