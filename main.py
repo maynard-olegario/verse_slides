94f6ad1f331cc2510b9128df98da787cf22c0ff5
maynard-olegario <maynard.olegario@distilnetworks.com>
2019-07-16 12:37:17 -0700

    checkin latest changes

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
 import biblegateway
 import slides
+import pprint
+import re
 
 def main():
+    pp = pprint.PrettyPrinter(indent=2)
+
     citations = get_citations('./verses.txt')
-    passages = get_passages(citations)
-    print(passages)
+    print('***** AFTER get_citations() *****')
+    pp.pprint (citations)
+
+    passages = []
+    for c in citations:
+        p = get_passage(c['citation'], c['version'])
+        passages.append(p)
+
+    print('***** AFTER get_passage() *****')
+    pp.pprint (passages)
+
     for p in passages:
-        slides.add_slide(p['reference'] + ' ' + p['version'], p['text'])
+        # print('***********')
+        # print(p)
+        slides.add_slide(str(p['reference']) + ' ' + str(p['version']), p['text'])
 
 def get_citations(filename):
     f = open(filename, 'r')
     citations = []
     for line in f:
-        citations.append(line)
+        version = None
+        line = line.strip()
+
+        # get version from end of citation
+        matchObj = re.search( r'(.*\d+\s*:.*\d+) *([a-zA-Z]+)$', line, re.I)
+        if matchObj:
+            book_chap_verse = matchObj.group(1)
+            version = matchObj.group(2)
+        else:
+            book_chap_verse = line
+            version = ''
+        citation = {'citation': book_chap_verse, 'version': version}
+        citations.append(citation)
     return citations
 
-def get_passages(citations):
-    default_version = 'NKJV'
+def get_passage(citation, version='NKJV'):
     INCLUDE_VERSE = True
     INCLUDE_TITLE = False
-    passages = []
-    for c in citations:
-        p = biblegateway.get_passage(c, default_version, INCLUDE_VERSE, INCLUDE_TITLE)
-        passages.append(p)
-    return passages
+    # print('citation=' + str(citation) + ' :: version=' + str(version))
+
+    if version == '':
+        version = 'NKJV'
+    elif version == 'TEV':
+        version = 'GNT'
+
+    passage = biblegateway.get_passage(citation, version, INCLUDE_VERSE, INCLUDE_TITLE)
+    if passage == 'empty':
+        passage = {'reference': citation, 'version': '', 'text': 'Error encountered fetching citation'}
+    return passage
 
 if __name__ == '__main__':
     main()
